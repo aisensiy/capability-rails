57e32eb19efdf70ca77932aeab7ee52905777ff8
xushanchuan <aisensiy@163.com>
2015-11-15 16:42:23 +0800

    valid status

--- spec/models/exam_request_spec.rb
+++ spec/models/exam_request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe ExamRequest, type: :model do
     @member = create :employee
     @tag = create :tag
     @exam_papter = @tag.exam_papers.create attributes_for(:exam_paper)
+    @request = @member.exam_requests.create(exam_time: Time.now + 1.days, tag_id: @tag.id)
   end
 
   it 'should create new exame_request with status created' do
@@ -12,5 +13,15 @@ RSpec.describe ExamRequest, type: :model do
     expect(request.status).to eq("created")
 
     expect(@member.exam_requests.size).to eq(1)
+  end
+
+  it 'should update failed without invalid status' do
+    @request.status = 'abc'
+    result = @request.save
+    expect(result).to be(false)
+  end
+
+  it 'should ' do
+    
   end
 end
